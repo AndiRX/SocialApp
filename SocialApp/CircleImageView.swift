ac6c447a11d7d93b0fc700a3c883172f7c9e0544
Petr <petr@petrs-air.localdomain>
2017-11-18 23:37:24 +0100

    intermediate ui-feed

--- SocialApp/CircleImageView.swift
+++ SocialApp/CircleImageView.swift
@@ -1,25 +1,27 @@
 //
-//  FancyView.swift
+//  CircleImageView.swift
 //  SocialApp
 //
-//  Created by Petr on 17.11.17.
+//  Created by Petr on 18.11.17.
 //  Copyright © 2017 Andi. All rights reserved.
 //
 
 import UIKit
 
-@IBDesignable
-class FancyView: UIView {
+class CircleImageView: UIImageView {
 
     override func awakeFromNib() {
         super.awakeFromNib()
         
         layer.shadowColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.6).cgColor
-        layer.shadowRadius = 5
-        layer.shadowOpacity = 0.9
+        layer.shadowRadius = 5.0
+        layer.shadowOpacity = 0.8
         layer.shadowOffset = CGSize.init(width: 1.0, height: 1.0)
-        
-        
     }
     
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        layer.cornerRadius = self.frame.width/2
+    }
+
 }
